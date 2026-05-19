d4a437e76973206e41ceba32ae8d4829cef98bf4
projcjdevs <cjcabatian5@gmail.com>
2026-05-19 23:54:58 +0800

    Fixed vector search logic (top_k)

--- src/cpnlookup/retrieval/vector_search.py
+++ src/cpnlookup/retrieval/vector_search.py
@@ -2,7 +2,7 @@ import numpy as np
 from cpnlookup.indexer.storage import load_faiss_index
 from cpnlookup.indexer.embedder import get_model
 
-def search_chunks(query: str, top_k, int=5):
+def search_chunks(query: str, top_k: int = 5):
     index = load_faiss_index()
     if not index:
         return[]
